c8de9ac3557ec4537581f771c7640705a9558794
Disney <331027793@qq.com>
2017-02-16 00:06:51 +0800

    Update auto-invite.py

--- examples/auto-invite.py
+++ examples/auto-invite.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import itchat
-from itchat.content import TEXT
+from itchat.content import *
 
 CHAT_ROOM = '2017IFE抱团群'
 TOKEN = 'IFE'
@@ -17,6 +17,13 @@ def text_reply(msg):
       r = itchat.add_member_into_chatroom(chatroom['UserName'], [friend])
       if r['BaseResponse']['ErrMsg'] == '':
         itchat.send(('自动邀请成功！'), msg['FromUserName'])
+
+# 收到好友邀请自动添加好友
+@itchat.msg_register(FRIENDS)
+def add_friend(msg):
+    print(msg)
+    itchat.add_friend(msg['RecommendInfo']['UserName']) # 该操作会自动将新好友的消息录入，不需要重载通讯录
+    itchat.send('自动通过好友请求!', msg['RecommendInfo']['UserName'])
 
 itchat.auto_login(enableCmdQR=True,hotReload=True)
 itchat.run()
