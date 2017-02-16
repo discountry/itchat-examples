2c6bd7d78d47f55ae5dacc41418b94e7b959c4ca
Disney <331027793@qq.com>
2017-02-16 18:22:54 +0800

    Update auto-invite.py

--- examples/auto-invite.py
+++ examples/auto-invite.py
@@ -20,43 +20,14 @@ def text_reply(msg):
       auto_add_member(msg,'2017IFE抱团群')
     if '签到' in msg['Text'] or '100days' in msg['Text']:
       auto_add_member(msg,'100days')
+    if 'fcc' in msg['Text'] or 'FCC' in msg['Text']:
+      auto_add_member(msg,'FCC知乎学习小组')
 
 # 收到好友邀请自动添加好友
 @itchat.msg_register(FRIENDS)
 def add_friend(msg):
         print(msg)
-        itchat.add_friend(msg['RecommendInfo']['UserName']) # 该操作会自动将新好友的消息录入，不需要重载通讯录
+        itchat.add_friend(msg['RecommendInfo']['UserName'],status=3,verifyContent='自动添加好友成功！') # 该操作会自动将新好友的消息录入，不需要重载通讯录
 
 itchat.auto_login(enableCmdQR=2,hotReload=False)
-itchat.run(debug=True)
-
-
-
-
-
-
-# CHAT_ROOM = '2017IFE抱团群'
-# TOKEN = 'IFE'
-
-# @itchat.msg_register(TEXT, isGroupChat=False)
-# def text_reply(msg):
-#     print(msg)
-#     if TOKEN in msg['Text']:
-#       friend = itchat.search_friends(userName=msg['FromUserName'])
-#       print(friend)
-#       itchat.get_chatrooms(update=True)
-#       chatroom = itchat.search_chatrooms(CHAT_ROOM)[0]
-#       print(chatroom)
-#       r = itchat.add_member_into_chatroom(chatroom['UserName'], [friend])
-#       if r['BaseResponse']['ErrMsg'] == '':
-#         itchat.send(('自动邀请成功！'), msg['FromUserName'])
-
-# # 收到好友邀请自动添加好友
-# @itchat.msg_register(FRIENDS)
-# def add_friend(msg):
-#     print(msg)
-#     itchat.add_friend(msg['RecommendInfo']['UserName']) # 该操作会自动将新好友的消息录入，不需要重载通讯录
-#     itchat.send('自动通过好友请求!', msg['RecommendInfo']['UserName'])
-
-# itchat.auto_login(enableCmdQR=True,hotReload=True)
-# itchat.run()
+itchat.run()
