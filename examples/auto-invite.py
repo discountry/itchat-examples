c19ea40fa83f39a675ad7c2653b6e8085770b748
Disney <331027793@qq.com>
2017-02-17 13:17:08 +0800

    Update auto-invite.py

--- examples/auto-invite.py
+++ examples/auto-invite.py
@@ -8,20 +8,22 @@ def auto_add_member(msg,roomName):
     itchat.get_chatrooms(update=True)
     chatroom = itchat.search_chatrooms(roomName)[0]
     print(chatroom['UserName'])
-    r = itchat.add_member_into_chatroom(chatroom['UserName'], [friend])
+    r = itchat.add_member_into_chatroom(chatroom['UserName'], [friend], useInvitation=True)
     print(r)
     if r['BaseResponse']['ErrMsg'] == '请求成功':
-      return '自动邀请加入群聊成功！'
+      return '自动邀请加入群聊成功！请等待获取加群链接！'
+    else:
+      return '请求发生错误，请重试！'
 
 @itchat.msg_register(TEXT)
 def text_reply(msg):
     print(msg)
     if 'IFE' in msg['Text'] or 'ife' in msg['Text']:
-      auto_add_member(msg,'2017IFE抱团群')
+      return auto_add_member(msg,'2017IFE抱团群')
     if '签到' in msg['Text'] or '100days' in msg['Text']:
-      auto_add_member(msg,'100days')
+      return auto_add_member(msg,'100days')
     if 'fcc' in msg['Text'] or 'FCC' in msg['Text']:
-      auto_add_member(msg,'FCC知乎学习小组')
+      return auto_add_member(msg,'FCC知乎学习小组')
 
 # 收到好友邀请自动添加好友
 @itchat.msg_register(FRIENDS)
@@ -29,5 +31,5 @@ def add_friend(msg):
         print(msg)
         itchat.add_friend(msg['RecommendInfo']['UserName'],status=3,verifyContent='自动添加好友成功！') # 该操作会自动将新好友的消息录入，不需要重载通讯录
 
-itchat.auto_login(enableCmdQR=2,hotReload=False)
+itchat.auto_login(enableCmdQR=2,hotReload=True)
 itchat.run()
